026cf528b62c88672a38835767e18476aa630f65
YongPilMoon <m71661@gmail.com>
2016-11-15 17:18:50 +0900

    Add sginup function

--- django-app/member/urls/views.py
+++ django-app/member/urls/views.py
@@ -1,7 +1,8 @@
 from django.conf.urls import url
-from ..views import login_fbv, Login
+from ..views import login_fbv, Login, Signup
 
 urlpatterns = [
     url(r'login/$', login_fbv, name='login_fbv'),
     url(r'login/class/$', Login.as_view(), name='login_cbv'),
+    url(r'signup/$', Signup.as_view(), name='signup'),
 ]
